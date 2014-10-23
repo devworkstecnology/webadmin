905f9b2b16a1ffed0fe764bd63b9e2ddd7eff98d
Rodrigo Ulisses E Silva <rodrigo.ulisses7@gmail.com>
2014-10-23 19:26:47 -0300

    Adicionando rspec e factory girl

--- Gemfile
+++ Gemfile
@@ -12,3 +12,6 @@ gemspec
 
 # To use debugger
 # gem 'debugger'
+
+gem "rspec-rails"
+gem "factory_girl_rails"
--- lib/admin_web/engine.rb
+++ lib/admin_web/engine.rb
@@ -1,5 +1,10 @@
 module AdminWeb
   class Engine < ::Rails::Engine
     isolate_namespace AdminWeb
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end
