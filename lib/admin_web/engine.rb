e2f0d90bbee371e4b48aabd538f72141a9669d4d
Rodrigo Ulisses E Silva <rodrigo.ulisses7@gmail.com>
2014-10-24 19:06:20 -0300

    Configurando a internacionalizacao do projeto.

--- lib/admin_web/engine.rb
+++ lib/admin_web/engine.rb
@@ -6,5 +6,10 @@ module AdminWeb
       g.test_framework :rspec
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
     end
+
+    initializer("admin_web.locales") do |app|
+      AdminWeb::Engine.config.i18n.load_path += Dir[Rails.root.join('../../config/locales/', '*.{rb,yml}').to_s]
+      AdminWeb::Engine.config.i18n.default_locale = 'pt-BR'
+    end
   end
 end
