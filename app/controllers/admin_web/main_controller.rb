f478a73dc31feb14ded13e0ef70d9906bc840268
Rodrigo Ulisses E Silva <rodrigo.ulisses7@gmail.com>
2014-10-24 11:53:25 -0300

    Criando controller principal.

---
+++ app/controllers/admin_web/main_controller.rb
@@ -0,0 +1,7 @@
+class AdminWeb::MainController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 AdminWeb::Engine.routes.draw do
   devise_for :users, class_name: "AdminWeb::User", module: :devise
+  root "main#index"
 end
