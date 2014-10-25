6343baec97e7e0bbec8d7d82c347d88745a80953
Rodrigo Ulisses E Silva <rodrigo.ulisses7@gmail.com>
2014-10-25 11:17:01 -0300

    Criando teste de feature para Login.

---
+++ spec/features/admin_web/login_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Login", :type => :feature, :js => true do
+  it "login" do
+    user = create(:admin_web_user)
+
+    visit admin_web.root_path
+
+    expect(page).to have_content "AdminWeb - Administração de sites"
+
+    fill_in 'Email', with: "user@email.com"
+    fill_in 'Senha', with: "12345678"
+
+    click_button 'Entrar'
+
+    expect(page).to have_content "Bem vindo"
+  end
+end
