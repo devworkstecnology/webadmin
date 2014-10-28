17ac710bd0d114c230c110deee2b0d2e3d618eb2
Rodrigo Ulisses E Silva <rodrigo.ulisses7@gmail.com>
2014-10-27 21:10:16 -0300

    Adicionando jquery e jquery_ujs, além de fontes do bootstrap

--- app/assets/javascripts/admin_web/application.js
+++ app/assets/javascripts/admin_web/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+ // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// jquery-1.11.1.min
-// bootstrap.min
-// require_tree .
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
