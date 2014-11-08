1db18ec2478f889b47dba9f8e408c64df6f9bc94
Rodrigo Ulisses E Silva <rodrigo.ulisses7@gmail.com>
2014-11-08 10:16:41 -0300

    Alterando tamanho das colunas no formulário.

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,10 +1,11 @@
+
 # Use this setup block to configure all options available in SimpleForm.
 SimpleForm.setup do |config|
   config.wrappers :bootstrap, tag: 'div', class: 'form-group control-group', error_class: 'error' do |b|
     b.use :html5
     b.use :placeholder
     b.use :label
-    b.wrapper tag: 'div', class: ' col-md-9' do |ba|
+    b.wrapper tag: 'div', class: ' col-md-10' do |ba|
       ba.use :input
       ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
       ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
