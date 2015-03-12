d1af64582de959840cfe4605ee3d840b16ba7bdb
Rodrigo Ulisses E Silva <rodrigo.ulisses7@gmail.com>
2015-03-12 16:57:36 -0300

    Atualizando poltergeits e adicionando js_erros false em sua configuração.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -39,7 +39,8 @@ end
 Capybara.register_driver :poltergeist do |app|
   options = {
     #The dimensions of the browser window in which to test, expressed as a 2-element array,
-    :window_size => [1280, 800]
+    :window_size => [1280, 800],
+    :js_errors => false
   }
 
   Capybara::Poltergeist::Driver.new(app, options)
@@ -49,4 +50,4 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
-Capybara.default_wait_time = 5
+Capybara.default_wait_time = 8
