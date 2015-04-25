9519c550214ed36cea04e9a983a14c47e9d4d89a
Rodrigo Ulisses E Silva <rodrigo.ulisses7@gmail.com>
2015-04-25 16:45:53 -0300

    Alterando teste de feature de usuário.
    Testando funcinalidade de dar permissões de uso do sistema aos usuários.

--- spec/features/admin_web/users_spec.rb
+++ spec/features/admin_web/users_spec.rb
@@ -15,6 +15,12 @@ feature AdminWeb::User do
     fill_in 'Email', with: "email@email.com"
     fill_in 'Senha', with: "12345678"
     fill_in 'Confirmação de senha', with: "12345678"
+    select 'Acesso total', from: 'user_permissions_attributes_0_permission_type'
+    select 'Acesso total', from: 'user_permissions_attributes_1_permission_type'
+    select 'Acesso total', from: 'user_permissions_attributes_2_permission_type'
+    select 'Acesso total', from: 'user_permissions_attributes_3_permission_type'
+    select 'Acesso total', from: 'user_permissions_attributes_4_permission_type'
+    select 'Acesso total', from: 'user_permissions_attributes_5_permission_type'
 
     click_button 'Salvar'
 
@@ -28,8 +34,20 @@ feature AdminWeb::User do
     expect(page).to have_field "Email", with: "email@email.com"
     expect(page).to have_field "Senha", with: ""
     expect(page).to have_field "Confirmação de senha", with: ""
+    expect(page).to have_select 'user_permissions_attributes_0_permission_type', selected: 'Acesso total'
+    expect(page).to have_select 'user_permissions_attributes_1_permission_type', selected: 'Acesso total'
+    expect(page).to have_select 'user_permissions_attributes_2_permission_type', selected: 'Acesso total'
+    expect(page).to have_select 'user_permissions_attributes_3_permission_type', selected: 'Acesso total'
+    expect(page).to have_select 'user_permissions_attributes_4_permission_type', selected: 'Acesso total'
+    expect(page).to have_select 'user_permissions_attributes_5_permission_type', selected: 'Acesso total'
 
     fill_in 'Email', with: "email1@email.com"
+    select 'Acesso total', from: 'user_permissions_attributes_0_permission_type'
+    select 'Apenas leitura', from: 'user_permissions_attributes_1_permission_type'
+    select 'Bloqueado', from: 'user_permissions_attributes_2_permission_type'
+    select 'Atualizar', from: 'user_permissions_attributes_3_permission_type'
+    select 'Acesso total', from: 'user_permissions_attributes_4_permission_type'
+    select 'Acesso total', from: 'user_permissions_attributes_5_permission_type'
 
     click_button "Salvar"
 
@@ -41,6 +59,12 @@ feature AdminWeb::User do
     click_link 'email1@email.com'
 
     expect(page).to have_field "Email", with: "email1@email.com"
+    expect(page).to have_select 'user_permissions_attributes_0_permission_type', selected: 'Acesso total'
+    expect(page).to have_select 'user_permissions_attributes_1_permission_type', selected: 'Apenas leitura'
+    expect(page).to have_select 'user_permissions_attributes_2_permission_type', selected: 'Bloqueado'
+    expect(page).to have_select 'user_permissions_attributes_3_permission_type', selected: 'Atualizar'
+    expect(page).to have_select 'user_permissions_attributes_4_permission_type', selected: 'Acesso total'
+    expect(page).to have_select 'user_permissions_attributes_5_permission_type', selected: 'Acesso total'
 
     click_link 'Apagar'
 
