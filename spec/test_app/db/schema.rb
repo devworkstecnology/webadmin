85e5b6c055f7c19ef9aa67f9a3c51d9922876bb0
Rodrigo Ulisses e Silva <rodrigo.ulisses7@gmail.com>
2016-01-24 10:59:58 -0300

    Creating schema.rb

---
+++ spec/test_app/db/schema.rb
@@ -0,0 +1,122 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150719130224) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "ckeditor_assets", force: true do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+
+  create_table "web_admin_albums", force: true do |t|
+    t.string   "title",      null: false
+    t.date     "date",       null: false
+    t.string   "local"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "web_admin_events", force: true do |t|
+    t.string   "title",        null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.text     "description",  null: false
+    t.string   "street",       null: false
+    t.string   "number"
+    t.string   "zipcode",      null: false
+    t.string   "neighborhood", null: false
+    t.string   "city",         null: false
+    t.string   "state",        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "image"
+  end
+
+  create_table "web_admin_images", force: true do |t|
+    t.integer  "album_id",   null: false
+    t.string   "file",       null: false
+    t.boolean  "cover"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "web_admin_institutionals", force: true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "web_admin_permissions", force: true do |t|
+    t.integer  "user_id"
+    t.string   "model"
+    t.string   "permission_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "web_admin_permissions", ["user_id"], name: "index_web_admin_permissions_on_user_id", using: :btree
+
+  create_table "web_admin_posts", force: true do |t|
+    t.integer  "user_id",    null: false
+    t.string   "title",      null: false
+    t.string   "abstract"
+    t.text     "body",       null: false
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "web_admin_posts", ["user_id"], name: "index_web_admin_posts_on_user_id", using: :btree
+
+  create_table "web_admin_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_type"
+  end
+
+  add_index "web_admin_users", ["email"], name: "index_web_admin_users_on_email", unique: true, using: :btree
+  add_index "web_admin_users", ["reset_password_token"], name: "index_web_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "web_admin_videos", force: true do |t|
+    t.string   "description"
+    t.string   "link"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
